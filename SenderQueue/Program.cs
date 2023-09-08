e0773f1f6b46610ba92f3dced2a3410e19013f62
Jitesh Karanjkar <Jitesh.Karanjkar@Nintex.com>
2023-09-08 21:51:04 +1000

    added servicebus queue projects

---
+++ SenderQueue/Program.cs
@@ -0,0 +1,56 @@
+﻿using Azure.Identity;
+using Azure.Messaging.ServiceBus;
+using Microsoft.Extensions.Azure;
+
+// name of your Service Bus queue
+// the client that owns the connection and can be used to create senders and receivers
+ServiceBusClient client;
+
+// the sender used to publish messages to the queue
+ServiceBusSender sender;
+
+// number of messages to be sent to the queue
+const int numOfMessages = 3;
+
+var clientOptions = new ServiceBusClientOptions
+{
+    TransportType = ServiceBusTransportType.AmqpWebSockets
+};
+client = new ServiceBusClient(
+    "jk-poc-service-bus-nsp.servicebus.windows.net",
+    new DefaultAzureCredential(),
+    clientOptions);
+sender = client.CreateSender("jk-poc-queue");
+
+// create a batch 
+using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+
+for (int i = 0; i < numOfMessages; i++)
+{
+    if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {1}")))
+    {
+        // if it is too large for the batch
+        throw new Exception($"The message {i} is too large to fit in the batch.");
+    }
+}
+
+try
+{
+    // Use the producer client to send the batch of messages to the Service Bus queue
+    await sender.SendMessagesAsync(messageBatch);
+    Console.WriteLine($"A batch of {numOfMessages} messages has been published to the queue.");
+}
+catch (Exception)
+{
+    throw;
+}
+finally
+{
+    // Calling DisposeAsync on client types is required to ensure that network
+    // resources and other unmanaged objects are properly cleaned up.
+    await sender.DisposeAsync();
+    await client.DisposeAsync();
+}
+Console.WriteLine("Press any key to end the application");
+Console.ReadKey();
+
